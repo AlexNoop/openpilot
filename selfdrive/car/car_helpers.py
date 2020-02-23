6905c6c658c9ac887922bd7826f623da2436ced6
AlexNoop <alex-77186@live.fr>
2020-02-23 12:54:30 +0100

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -204,6 +204,8 @@ def get_car(logcan, sendcan, has_relay=False):
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
     candidate = "mock"
+    
+    candidate = "HONDA CIVIC HATCHBACK 2017 SEDAN/COUPE 2019"
 
   CarInterface, CarController = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
