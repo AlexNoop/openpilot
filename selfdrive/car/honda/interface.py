e568d47b5eff79baf381e40c738b6fd03cd0bc60
AlexNoop <alex-77186@live.fr>
2020-03-20 07:26:54 +0100

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -381,6 +381,8 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.1
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.8
+    
+    ret.transmissionType = car.CarParams.TransmissionType.manual
 
     return ret
 
